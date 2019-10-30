cd3c334892cb29c8cd2fa71fa73acdd19bc06ca0
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-10-30 01:54:45 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,6 +17,11 @@ ENFIELD_TENNIS_ACADEMY = [
 ]
 
 def assembled_matrix
+  ENNET_HOUSE_AND_ENFIELD_TENNIS_ACADEMY =[
+    ["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"],
+  ["Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza", "Michael Pemulis"]
+]
+ENNET_HOUSE_AND_ENFIELD_TENNIS_ACADEMY
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
 end
